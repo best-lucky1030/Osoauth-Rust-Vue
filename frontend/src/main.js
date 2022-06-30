8c3c0ad43c331a9456e2de74725863d7ed826c66
best-lucky1030 <bestlucky1030@gmail.com>
2022-06-30 14:33:52 +0300

    update frontend

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,6 +1,11 @@
-import { createApp } from 'vue'
+import Vue from 'vue'
 import App from './App.vue'
-import router from './router'
-import store from './store'
 
-createApp(App).use(store).use(router).mount('#app')
+import bootstrap from 'bootstrap/dist/css/bootstrap.css'
+
+Vue.config.productionTip = false
+
+new Vue({
+  render: h => h(App),
+  bootstrap,
+}).$mount('#app')
